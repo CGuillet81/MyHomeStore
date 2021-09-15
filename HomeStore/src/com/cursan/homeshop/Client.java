8a77bf036a8d783f2b13b3c94461a86aa92173fb
cg068 <cg0681571909@gmail.com>
2021-09-15 17:33:34 +0200

    First commit

---
+++ HomeStore/src/com/cursan/homeshop/Client.java
@@ -0,0 +1,14 @@
+package com.cursan.homeshop;
+
+public class Client {
+    private String fullname;
+    private String address;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
